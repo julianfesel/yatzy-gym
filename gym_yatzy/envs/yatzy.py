85c33917837a0bc62be3947bc65f37eb0c0283bd
JulianFesel <julian.fesel@oebb.at>
2022-11-27 17:54:09 +0100

    Update to use gymnasium instead of gym

--- gym_yatzy/envs/yatzy.py
+++ gym_yatzy/envs/yatzy.py
@@ -1,7 +1,7 @@
 import numpy as np
-import gym
-from gym import spaces
-from gym.utils import seeding
+import gymnasium as gym
+from gymnasium import spaces
+from gymnasium.utils import seeding
 import sys
 from typing import List, Dict, Tuple, Union, Any, Optional
 
--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_yatzy',
       version='0.0.1',
-      install_requires=['gym', 'numpy']
+      install_requires=['gymnasium', 'numpy']
 )
